226ff430fa0c1ac6f03395cb0ca677dbef4351c2
ellenolv <ellendeoliverapereira@gmail.com>
2023-04-11 10:21:03 -0300

    quinto commit

--- OneDrive/Documentos/ADS/Terceiro semestre/WEB/prova/prova1/src/app/conversor/conversor.component.ts
+++ OneDrive/Documentos/ADS/Terceiro semestre/WEB/prova/prova1/src/app/conversor/conversor.component.ts
@@ -20,64 +20,18 @@ export class ConversorComponent {
   verificacao1() {
     const numeroConvertido1: number = parseInt(this.opcao1); //Converte a strig para numero
     const numeroConvertido2: number = parseInt(this.opcao2); //Converte a strig para numero
-    //verificar se foi selecionado alguma moeda
-    if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
-      //if com todas as possibilidade do primeiro imput
-      if (numeroConvertido1 == 1 && numeroConvertido2 == 1) { // de real para real
-        this.result = this.ValorCambio = this.ValorDesejado;
-      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) { 
-        //real para Dolar
-        this.result = this.ValorCambio * this.ValorDesejado;
-      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
-        this.ValorCambio = this.ValorDesejado * 100;
-      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
-        this.ValorCambio = this.ValorDesejado * 1000;
-      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
-        this.ValorCambio = this.ValorDesejado;
-      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
-        this.ValorCambio = this.ValorDesejado * 100000;
-      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
-        this.ValorCambio = this.ValorDesejado / 100;
-      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
-        this.ValorCambio = this.ValorDesejado / 100000;
-      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
-        this.ValorCambio = this.ValorDesejado * 100000;
-      } else {
-        this.setMesagemError('Operação invalida');
-      }
-    } else {
-      this.setMesagemError('Selecione a moeda dos dois campos!');
-    }
-  }
-  verificacao2() {
-    const numeroConvertido1: number = parseInt(this.opcao1); //Converte a strig para numero
-    const numeroConvertido2: number = parseInt(this.opcao2); //Converte a strig para numero
     //verificar se foi selecionado alguma medida
     if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
       //if com todas as possibilidade do primeiro imput
-      if (numeroConvertido1 == 1 && numeroConvertido2 == 1) {
-        this.ValorDesejado= this.ValorCambio;
-      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) {
-        this.ValorDesejado = this.ValorCambio / 1000;
-      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
-        this.ValorDesejado = this.ValorCambio * 100;
-      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
-        this.ValorDesejado = this.ValorCambio * 1000;
-      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
-        this.ValorDesejado = this.ValorCambio;
-      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
-        this.ValorDesejado = this.ValorCambio * 100000;
-      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
-        this.ValorDesejado = this.ValorCambio / 100;
-      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
-        this.ValorDesejado = this.ValorCambio / 100000;
-      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
-        this.ValorDesejado = this.ValorCambio * 100000;
-      } else {
-        this.setMesagemError('Operação invalida');
+      if (numeroConvertido1 == numeroConvertido2) { // de moedas iguais
+        this.result = this.ValorCambio;
+      } else{ 
+        //
+        this.result =  this.ValorDesejado / this.ValorCambio ;
+      
       }
     } else {
-      this.setMesagemError('Selecione a medida dos dois campos!');
+      this.setMesagemError('Selecione a moeda dos dois campos!');
     }
   }
 
