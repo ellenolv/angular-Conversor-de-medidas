931ea04a93082524d70f131e461d7ecab6883d33
ellenolv <ellendeoliverapereira@gmail.com>
2023-04-11 11:09:37 -0300

    setimo commit

--- OneDrive/Documentos/ADS/Terceiro semestre/WEB/prova/prova1/src/app/conversor/conversor.component.ts
+++ OneDrive/Documentos/ADS/Terceiro semestre/WEB/prova/prova1/src/app/conversor/conversor.component.ts
@@ -25,32 +25,18 @@ export class ConversorComponent {
       //if com todas as possibilidade do primeiro imput
       if (numeroConvertido1 == numeroConvertido2) { // de real para real
         this.result = this.ValorCambio;
-      } else{ 
+      } else if(numeroConvertido1 == 1 && numeroConvertido2 == 2){ 
         //real para Dolar
         this.result =  this.ValorDesejado / this.ValorCambio ;
       } 
-    } else {
+      else if(numeroConvertido1 == 2 && numeroConvertido2 ==1){ 
+        //  Dolar para real
+        this.result =  this.ValorDesejado * this.ValorCambio ;
+      } 
+    } else{
       this.setMesagemError('Selecione a moeda dos dois campos!');
     }
   }
-  verificacao2() {
-    const numeroConvertido1: number = parseInt(this.opcao1); //Converte a strig para numero
-    const numeroConvertido2: number = parseInt(this.opcao2); //Converte a strig para numero
-    //verificar se foi selecionado alguma medida
-    if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
-      //if com todas as possibilidade do primeiro imput
-      if (numeroConvertido1 == numeroConvertido2) { // de moedas iguais
-        this.result = this.ValorCambio;
-      } else{ 
-        //
-        this.result =  this.ValorDesejado / this.ValorCambio ;
-      
-      }
-    } else {
-      this.setMesagemError('Selecione a moeda dos dois campos!');
-    }
-  }
-
   //função para mostrar mensagem de erro
   setMesagemError(mensagem: string) {
     this.mensagemError = mensagem;
