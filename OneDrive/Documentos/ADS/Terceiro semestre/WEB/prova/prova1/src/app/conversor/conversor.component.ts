c255d6f069eda76ef7005943ff60fd1e64685150
ellenolv <ellendeoliverapereira@gmail.com>
2023-04-11 08:40:22 -0300

    primeiro commit

---
+++ OneDrive/Documentos/ADS/Terceiro semestre/WEB/prova/prova1/src/app/conversor/conversor.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conversor',
+  templateUrl: './conversor.component.html',
+  styleUrls: ['./conversor.component.css']
+})
+export class ConversorComponent{
+
+}
