daab801b7897cbc618c53e11000c27f1ec3639dc
ellenolv <ellendeoliverapereira@gmail.com>
2023-04-11 09:55:49 -0300

    quarto commit

--- OneDrive/Documentos/ADS/Terceiro semestre/WEB/prova/prova1/src/app/conversor/conversor.component.ts
+++ OneDrive/Documentos/ADS/Terceiro semestre/WEB/prova/prova1/src/app/conversor/conversor.component.ts
@@ -6,20 +6,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversor.component.css']
 })
 export class ConversorComponent {
-  //valor desejado
-  Vdesejad : number = 78;
-  Vcambio : number = 78;
-  result : number = 59;
+  // variaveis
+  opcao1: string = 'Moedas';
+  opcao2: string = 'Moedas';
 
+  //numeros digitados
+  ValorDesejado!: number;
+  ValorCambio!: number;
+  result! : number;
 
-  //opção selecionada
-   calc(){
+  mensagemError: string = '';
 
-   }
-   limpar(){
-    this.Vcambio = 0;
-    this.Vdesejad = 0;
-    this.result = 0;
+  verificacao1() {
+    const numeroConvertido1: number = parseInt(this.opcao1); //Converte a strig para numero
+    const numeroConvertido2: number = parseInt(this.opcao2); //Converte a strig para numero
+    //verificar se foi selecionado alguma moeda
+    if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
+      //if com todas as possibilidade do primeiro imput
+      if (numeroConvertido1 == 1 && numeroConvertido2 == 1) { // de real para real
+        this.result = this.ValorCambio = this.ValorDesejado;
+      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) { 
+        //real para Dolar
+        this.result = this.ValorCambio * this.ValorDesejado;
+      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
+        this.ValorCambio = this.ValorDesejado * 100;
+      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
+        this.ValorCambio = this.ValorDesejado * 1000;
+      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
+        this.ValorCambio = this.ValorDesejado;
+      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
+        this.ValorCambio = this.ValorDesejado * 100000;
+      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
+        this.ValorCambio = this.ValorDesejado / 100;
+      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
+        this.ValorCambio = this.ValorDesejado / 100000;
+      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
+        this.ValorCambio = this.ValorDesejado * 100000;
+      } else {
+        this.setMesagemError('Operação invalida');
+      }
+    } else {
+      this.setMesagemError('Selecione a moeda dos dois campos!');
+    }
   }
-  
+  verificacao2() {
+    const numeroConvertido1: number = parseInt(this.opcao1); //Converte a strig para numero
+    const numeroConvertido2: number = parseInt(this.opcao2); //Converte a strig para numero
+    //verificar se foi selecionado alguma medida
+    if (!isNaN(numeroConvertido1) && !isNaN(numeroConvertido2)) {
+      //if com todas as possibilidade do primeiro imput
+      if (numeroConvertido1 == 1 && numeroConvertido2 == 1) {
+        this.ValorDesejado= this.ValorCambio;
+      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 2) {
+        this.ValorDesejado = this.ValorCambio / 1000;
+      } else if (numeroConvertido1 == 1 && numeroConvertido2 == 3) {
+        this.ValorDesejado = this.ValorCambio * 100;
+      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 1) {
+        this.ValorDesejado = this.ValorCambio * 1000;
+      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 2) {
+        this.ValorDesejado = this.ValorCambio;
+      } else if (numeroConvertido1 == 2 && numeroConvertido2 == 3) {
+        this.ValorDesejado = this.ValorCambio * 100000;
+      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 1) {
+        this.ValorDesejado = this.ValorCambio / 100;
+      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 2) {
+        this.ValorDesejado = this.ValorCambio / 100000;
+      } else if (numeroConvertido1 == 3 && numeroConvertido2 == 3) {
+        this.ValorDesejado = this.ValorCambio * 100000;
+      } else {
+        this.setMesagemError('Operação invalida');
+      }
+    } else {
+      this.setMesagemError('Selecione a medida dos dois campos!');
+    }
+  }
+
+  //função para mostrar mensagem de erro
+  setMesagemError(mensagem: string) {
+    this.mensagemError = mensagem;
+  }
+
+  limpar(){
+    this.mensagemError = '';
+  }
 }
